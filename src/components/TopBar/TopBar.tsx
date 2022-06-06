3cb41712ecebac2ce1e68b8de466083c4b283c5b
Patryk <byszek.patryk@gmail.com>
2022-06-07 01:02:05 +0200

    Initial setup of the component

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,7 +1,24 @@
-/*
-  icon for 'user avatar' can be found here: https://mui.com/material-ui/material-icons/
- */
+import * as React from "react"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import IconButton from "@mui/material/IconButton"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+import SettingsIcon from "@mui/icons-material/Settings"
 
 export const TopBar = () => {
-    return <div>Top bar component</div>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" sx={{ marginLeft: "auto" }}>
+            <SettingsIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <AccountCircleIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
 }
