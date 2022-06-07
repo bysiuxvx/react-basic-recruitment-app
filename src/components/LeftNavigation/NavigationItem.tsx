a4b362fd6f33ead50a8858741ea621acec8704b6
Patryk <byszek.patryk@gmail.com>
2022-06-08 01:07:19 +0200

    Added padding to the icon component

--- src/components/LeftNavigation/NavigationItem.tsx
+++ src/components/LeftNavigation/NavigationItem.tsx
@@ -1,21 +1,21 @@
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { SvgIconComponent } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import { FC, ReactElement } from "react";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { SvgIconComponent } from "@mui/icons-material"
+import { Link } from "react-router-dom"
+import { FC, ReactElement } from "react"
 
 type NavigationItemProp = {
-  to: string;
-  icon: ReactElement<SvgIconComponent>;
-  label: string;
-};
+  to: string
+  icon: ReactElement<SvgIconComponent>
+  label: string
+}
 
 export const NavigationItem: FC<NavigationItemProp> = ({ to, icon, label }) => {
   return (
     <Link to={to}>
       <ListItemButton>
-        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemIcon sx={{ paddingLeft: "1rem" }}>{icon}</ListItemIcon>
         <ListItemText>{label}</ListItemText>
       </ListItemButton>
     </Link>
-  );
-};
+  )
+}
