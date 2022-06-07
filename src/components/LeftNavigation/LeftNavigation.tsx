2daa5a0412baaca94219a9df2211889672e730c5
Patryk <byszek.patryk@gmail.com>
2022-06-08 01:06:35 +0200

    Initial setup, imports, initial alignment

--- src/components/LeftNavigation/LeftNavigation.tsx
+++ src/components/LeftNavigation/LeftNavigation.tsx
@@ -1,24 +1,87 @@
-import { Divider } from "@mui/material";
-import { Link } from "react-router-dom";
-import { navigationRoutes } from "../../navigationRoutes";
+import { Divider } from "@mui/material"
+import { Link } from "react-router-dom"
+import Box from "@mui/material/Box"
+import { Typography } from "@mui/material"
+import { navigationRoutes } from "../../navigationRoutes"
+import { NavigationItem } from "./NavigationItem"
+
+import HomeIcon from "@mui/icons-material/Home"
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer"
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents"
+import GroupsIcon from "@mui/icons-material/Groups"
+import FactCheckIcon from "@mui/icons-material/FactCheck"
+import LocationCityIcon from "@mui/icons-material/LocationCity"
 /*
   icons can be found in here: https://mui.com/material-ui/material-icons/
  */
 export const LeftNavigation = () => {
   return (
-    <>
-      <p>Management</p>
-      <Link to={navigationRoutes.dashboard.path}>Dashboard</Link>
-      <Link to={""}>Sports</Link>
-      <Link to={""}>Competitions</Link>
+    <Box
+      sx={{
+        height: "100vh",
+        width: 300,
+        borderRight: 2,
+      }}
+    >
+      <Box paddingTop={"1.5rem"}>
+        <Typography
+          sx={{
+            paddingLeft: "2rem",
+          }}
+        >
+          Management
+        </Typography>
+        <NavigationItem
+          to={navigationRoutes.dashboard.path}
+          icon={<HomeIcon />}
+          label={"Dashboard"}
+        />
+        <NavigationItem
+          to={navigationRoutes.sports.path}
+          icon={<SportsSoccerIcon />}
+          label={"Sports"}
+        />
+        <NavigationItem
+          to={navigationRoutes.competitions.path}
+          icon={<EmojiEventsIcon />}
+          label={"Competitions"}
+        />
+      </Box>
       <Divider />
-      <p>Planning</p>
-      <Link to={""}>Scheduling</Link>
-      <Link to={""}>Organisations</Link>
+      <Box paddingTop={"1.5rem"}>
+        <Typography
+          sx={{
+            paddingLeft: "2rem",
+          }}
+        >
+          Planning
+        </Typography>
+        <NavigationItem
+          to={navigationRoutes.scheduling.path}
+          icon={<FactCheckIcon />}
+          label={"Scheduling"}
+        />
+        <NavigationItem
+          to={navigationRoutes.organisations.path}
+          icon={<LocationCityIcon />}
+          label={"Organisations"}
+        />
+      </Box>
       <Divider />
-      <p>People</p>
-      <Link to={""}>Scheduling</Link>
-      <Link to={""}>Organisations</Link>
-    </>
-  );
-};
+      <Box paddingTop={"1.5rem"}>
+        <Typography
+          sx={{
+            paddingLeft: "2rem",
+          }}
+        >
+          People
+        </Typography>
+        <NavigationItem
+          to={navigationRoutes.users.path}
+          icon={<GroupsIcon />}
+          label={"Users"}
+        />
+      </Box>
+    </Box>
+  )
+}
