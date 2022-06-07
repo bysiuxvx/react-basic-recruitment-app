b6eb41ee2097f350d880b54eea897ea92c21c7bc
Patryk <byszek.patryk@gmail.com>
2022-06-08 00:51:32 +0200

    Updated the routes, init redirecting to dashboard

--- src/navigationRoutes.tsx
+++ src/navigationRoutes.tsx
@@ -1,11 +1,11 @@
-import { DashboardScreen } from "./screens/Dashboard";
-import { SportsScreen } from "./screens/Sports";
+import { DashboardScreen } from "./screens/Dashboard"
+import { SportsScreen } from "./screens/Sports"
 
 type NavigationRoute = {
-  path: string;
-  element: JSX.Element;
-};
-type NavigationRoutes = Record<string, NavigationRoute>;
+  path: string
+  element: JSX.Element
+}
+type NavigationRoutes = Record<string, NavigationRoute>
 
 export const navigationRoutes: NavigationRoutes = {
   dashboard: {
@@ -16,4 +16,20 @@ export const navigationRoutes: NavigationRoutes = {
     path: "/sports",
     element: <SportsScreen />,
   },
-};
+  competitions: {
+    path: "/competitions",
+    element: <DashboardScreen />,
+  },
+  scheduling: {
+    path: "/scheduling",
+    element: <DashboardScreen />,
+  },
+  organisations: {
+    path: "/organisations",
+    element: <DashboardScreen />,
+  },
+  users: {
+    path: "/users",
+    element: <DashboardScreen />,
+  },
+}
