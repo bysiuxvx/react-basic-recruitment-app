8603652872acaf518e14d78fcf956f37c72e10f3
Patryk <byszek.patryk@gmail.com>
2022-06-09 22:59:11 +0200

    Work on Sports comp, split body into diff comp

--- src/screens/Sports.tsx
+++ src/screens/Sports.tsx
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
-import { SportsType, SportType } from "../types/sports.types";
-import { NoResults } from "../components/NoResults/NoResults";
-import { TableColumn } from "../components/Table/Table";
-import { Visibility } from "@mui/icons-material";
+import { useEffect, useState } from "react"
+import { SportsType, SportType } from "../types/sports.types"
+import { NoResults } from "../components/NoResults/NoResults"
+import { TableColumn } from "../components/Table/Table"
+import { Visibility } from "@mui/icons-material"
+import { getSportById, getSports } from "../service/sports.service"
+
+import { Grid, Table, Typography } from "@mui/material"
+
+import SportsBody from "./SportsBody"
 
 export const SportsScreen = () => {
-  const [sports, setSports] = useState<SportsType | undefined>(undefined);
-  const [sportDetails, setSportDetails] = useState<SportType | undefined>(undefined);
+  const [sports, setSports] = useState<SportsType | undefined>(undefined)
+  const [sportDetails, setSportDetails] = useState<SportType | undefined>(
+    undefined
+  )
 
   const columns: TableColumn<SportType>[] = [
     { id: "sport", label: "Sport", value: "name" },
@@ -18,20 +25,37 @@ export const SportsScreen = () => {
       value: <Visibility />,
       textAlign: "right",
     },
-  ];
+  ]
 
-  const getSportDetails = (id: SportType['id']) => {
-    // TODO: get sport details
-  }
+  const getSportDetails = async (id: SportType["id"]) => await getSportById(id)
 
   useEffect(() => {
-    // TODO: get data from sports.service
-  }, []);
+    const fetchSportsData = async () => await getSports()
+    fetchSportsData()
+      .then((data) => setSports(data))
+      .catch((error) => console.log(error))
+  }, [setSports])
 
   if (!sports) {
-    return <NoResults />;
+    return <NoResults />
   }
 
-  // TODO: display data got form service
-  return <div>Sports page</div>;
-};
+  // return (
+  //   <Grid container p={8} pl={12}>
+  //     <Typography fontWeight={"bold"}>Sports</Typography>
+  //     <Typography pt={2}>{sports.teaser}</Typography>
+
+  //     <Grid item>
+  //     <Table
+  //           title={"Sports"}
+  //           columns={columns}
+  //           items={sports.items}
+  //           ButtonProps={{
+  //             children: "ADD SPORT",
+  //           }}
+  //         />
+  //     </Grid>
+  //   </Grid>
+  // )
+  return <SportsBody sports={sports} columns={columns} />
+}
