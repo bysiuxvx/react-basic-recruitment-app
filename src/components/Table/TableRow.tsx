914927d4cab448f7a1a30a87c7ee68961471ef44
Patryk <byszek.patryk@gmail.com>
2022-06-12 01:29:36 +0200

    added styling to table

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,34 +1,39 @@
-import React, { ReactElement } from "react";
-import { TableColumn } from "./Table";
-import { TableCell, TableRow as MuiTableRow } from "@mui/material";
-import { ModelWithId } from "../../types/table.types";
+import React, { ReactElement } from "react"
+import { TableColumn } from "./Table"
+import { useTheme, TableCell, TableRow as MuiTableRow } from "@mui/material"
+import { ModelWithId } from "../../types/table.types"
 
 type TableRowProps<Model> = {
-  item: Model;
-  columns: TableColumn<Model>[];
-};
+  item: Model
+  columns: TableColumn<Model>[]
+}
 
 export const TableRow = <Model extends ModelWithId>({
   item,
   columns,
 }: TableRowProps<Model>): JSX.Element => {
+  const theme = useTheme()
+
   const getItemContent = (
     column: TableColumn<Model>
   ): ReactElement | string => {
     if (React.isValidElement(column.value)) {
-      return column.value;
+      return column.value
     }
 
-    return item[column.value] as unknown as string;
-  };
+    return item[column.value] as unknown as string
+  }
 
   return (
     <MuiTableRow>
       {columns.map((column) => (
-        <TableCell sx={{ textAlign: column.textAlign || "left" }}>
+        <TableCell
+          sx={{ textAlign: column.textAlign || "left" }}
+          color={theme.palette.text.primary}
+        >
           {getItemContent(column)}
         </TableCell>
       ))}
     </MuiTableRow>
-  );
-};
+  )
+}
