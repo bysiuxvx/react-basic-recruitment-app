dc6e0ed118d8585bc15c5b42a183a1c154ebe6d7
Patryk <byszek.patryk@gmail.com>
2022-06-13 03:15:30 +0200

    Possible to select a sport in the table

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,24 +1,58 @@
-import React, { ReactElement } from "react"
+import React, {
+  useState,
+  ReactElement,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react"
+import {
+  useTheme,
+  TableCell,
+  TableRow as MuiTableRow,
+  IconButton,
+} from "@mui/material"
 import { TableColumn } from "./Table"
-import { useTheme, TableCell, TableRow as MuiTableRow } from "@mui/material"
 import { ModelWithId } from "../../types/table.types"
 
 type TableRowProps<Model> = {
   item: Model
   columns: TableColumn<Model>[]
+  activeSport: string | number | undefined
+  setActiveSport: Dispatch<SetStateAction<string | number | undefined>>
 }
 
 export const TableRow = <Model extends ModelWithId>({
   item,
   columns,
+  setActiveSport,
+  activeSport,
 }: TableRowProps<Model>): JSX.Element => {
   const theme = useTheme()
+
+  const [iconActive, setIconActive] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (activeSport && item["id"] !== activeSport) setIconActive(false)
+  }, [activeSport, item])
+
+  const test = theme.palette.secondary.contrastText
 
   const getItemContent = (
     column: TableColumn<Model>
   ): ReactElement | string => {
     if (React.isValidElement(column.value)) {
-      return column.value
+      return (
+        <IconButton
+          color={iconActive ? "primary" : "inherit"}
+          onClick={() => {
+            setActiveSport(item["id"])
+            setIconActive(true)
+          }}
+        >
+          {/* {activeSport} */}
+          {column.value}
+        </IconButton>
+      )
     }
 
     return item[column.value] as unknown as string
@@ -28,9 +62,16 @@ export const TableRow = <Model extends ModelWithId>({
     <MuiTableRow>
       {columns.map((column) => (
         <TableCell
+          onClick={() => {
+            setActiveSport(item["id"])
+            setIconActive(true)
+          }}
           sx={{
             textAlign: column.textAlign || "left",
             color: theme.palette.text.primary,
+            "&:hover": {
+              cursor: "pointer",
+            },
           }}
         >
           {getItemContent(column)}
